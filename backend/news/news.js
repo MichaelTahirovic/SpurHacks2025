61d517eea98d0cf57f41c4f13dccdf3215e56920
edwardhe10 <88951771+edwardhe10@users.noreply.github.com>
2025-06-21 14:50:27 -0400

    news article fetch test

---
+++ backend/news/news.js
@@ -0,0 +1,45 @@
+const https = require('https');
+const querystring = require('querystring');
+
+const API_TOKEN = '3Xb8vwsYNZAri7v8TMMMAZ7ekugTVxYsLn9EhRyu'; // Change
+const keywords = ['Trump', 'milk', 'cereal']; // Temp for testing
+const combinedSearch = keywords.join(' ');
+console.log(combinedSearch); // Remove
+
+const query = querystring.stringify({
+  api_token: API_TOKEN,
+  search: combinedSearch,
+  locale: 'us',
+  language: 'en',
+});
+console.log(query); // Testing
+
+const url = `https://api.thenewsapi.com/v1/news/all?${query}`;
+
+https.get(url, (res) => {
+    let data = '';
+  
+    res.on('data', chunk => data += chunk);
+    res.on('end', () => {
+      try {
+        const json = JSON.parse(data);
+        articles = json.data
+        if (!json.data) {
+          console.error('No articles found.');
+          return;
+        }
+  
+        articles.forEach((article, i) => {
+            console.log(`${i + 1}. ${article.title}`);
+            console.log(`   Source: ${article.source}`);
+            console.log(`   ${article.url}\n`);
+          });
+      } catch (err) {
+        console.error('JSON parse error:', err.message);
+      }
+    });
+  }).on('error', (err) => {
+    console.error('HTTPS error:', err.message);
+  });
+
+  
